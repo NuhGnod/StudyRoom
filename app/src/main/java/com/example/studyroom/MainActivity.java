a796e901f4ce8f05f716b67b309aaba57b2c860a
donghun <zhfptm12@naver.com>
2020-04-10 12:31:33 +0900

    StudyRoom
    StudyRoom

--- app/src/main/java/com/example/studyroom/MainActivity.java
+++ app/src/main/java/com/example/studyroom/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.studyroom;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -10,6 +12,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     private BackPressCloseHandler backPressCloseHandler = new BackPressCloseHandler(this);
@@ -20,6 +28,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+//
+//        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+//        if (user != null) {
+//            // Name, email address, and profile photo Url
+//            String name = user.getDisplayName();
+//            String email = user.getEmail();
+//            Uri photoUrl = user.getPhotoUrl();
+//
+//            // Check if user's email is verified
+//            boolean emailVerified = user.isEmailVerified();
+//
+//            // The user's ID, unique to the Firebase project. Do NOT use this value to
+//            // authenticate with your backend server, if you have one. Use
+//            // FirebaseUser.getIdToken() instead.
+//            String uid = user.getUid();
+//        }
+
         Button button = findViewById(R.id.go_room1_button);
         Button button2 = findViewById(R.id.go_room1_button2);
         Button button3 = findViewById(R.id.go_room1_button3);
@@ -37,16 +62,10 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(intent);
                 }
             });
-//        }
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(getApplicationContext(), seatActivity.class);
-//                startActivity(intent);
-//            }
-//        });
+
         }
     }
+
 
     @Override
     public void onBackPressed() {//뒤로가기 버튼 클릭시 종료
