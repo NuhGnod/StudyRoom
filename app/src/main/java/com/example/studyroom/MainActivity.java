cbd65c1ccc9cb4214662363b63ec875bbf2a5004
donghun <zhfptm12@naver.com>
2020-04-14 10:31:49 +0900

    StudyRoom
    firebase database

--- app/src/main/java/com/example/studyroom/MainActivity.java
+++ app/src/main/java/com/example/studyroom/MainActivity.java
@@ -14,14 +14,24 @@ import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private BackPressCloseHandler backPressCloseHandler = new BackPressCloseHandler(this);
     static Handler handler = new Handler();
+    private static String TAG = "DataBase";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +54,45 @@ public class MainActivity extends AppCompatActivity {
 //            // FirebaseUser.getIdToken() instead.
 //            String uid = user.getUid();
 //        }
+        // Access a Cloud Firestore instance from your Activity
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        // Create a new user with a first and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
+
+// Add a new document with a generated ID
+        db.collection("user")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+        db.collection("user")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                Log.d(TAG, "last : " + document.get("last"));
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+
+                });
 
         Button button = findViewById(R.id.go_room1_button);
         Button button2 = findViewById(R.id.go_room1_button2);
@@ -52,6 +101,7 @@ public class MainActivity extends AppCompatActivity {
         buttons[0] = button;
         buttons[1] = button2;
         buttons[2] = button3;
+
         for (int i = 0; i < 3; i++) {
             final int finalI = i;
             buttons[i].setOnClickListener(new View.OnClickListener() {
