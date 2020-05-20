ce373b83961297f21ad64703dba5ec2c8a8a3d70
donghun <zhfptm12@naver.com>
2020-05-20 23:51:30 +0900

    StudyRoom
    firestore 을 통해 메시지 주고받기

--- app/src/main/java/com/example/studyroom/FragmentChat.java
+++ app/src/main/java/com/example/studyroom/FragmentChat.java
@@ -16,6 +16,7 @@ import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import androidx.annotation.LongDef;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -37,6 +38,7 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.model.value.TimestampValue;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -53,7 +55,7 @@ public class FragmentChat extends Fragment {
     public RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     private List<ChatData> chatDataList;
-    private String nickname;
+    private String my_nickname;
     private String TAG = "FragmentChat_TAG";
     private EditText editText_chat;
     private Button button_send;
@@ -68,49 +70,59 @@ public class FragmentChat extends Fragment {
         db = FirebaseFirestore.getInstance();
         chatDataList = new ArrayList<>();
         SharedPreferences pref = getActivity().getSharedPreferences("userNickName", Context.MODE_PRIVATE);//해당 기기의 로그인된 아이디의 닉네임을 가져온다.
-        nickname = pref.getString("userNickName", null);
-        Log.d(TAG, "nickname : " + nickname);
+        my_nickname = pref.getString("userNickName", null);
+        Log.d(TAG, "nickname : " + my_nickname);
         //        @@@나중에 document 수정 해야함
         colRef = db.collection("chat").document("2").collection("message");
 //        @@@나중에 document "2" -> nickname 으로 수정 해야함
-
         button_send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String msg = editText_chat.getText().toString();
                 SimpleDateFormat SDF = new SimpleDateFormat("yyyy-MM-dd aa h:mm:ss:SS");
+                Date date = new Date(System.currentTimeMillis());
                 curTime = SDF.format(new Date(System.currentTimeMillis()));
                 String chat_time;
 //                Log.d(TAG, "msg : " + msg);
                 if (!msg.equals("")) {
                     ChatData chatData = new ChatData();
-                    chatData.setNickname(nickname);
+                    chatData.setNickname(my_nickname);
                     chatData.setContent(msg);
                     chatData.setTime(curTime);
                     chat_time = curTime.substring(11, 18);
                     chatData.setChat_time(chat_time);
                     //채팅 데이터 에 담기
-
 //                    Log.d(TAG, "chat_time : " + chat_time);
 
 //                    나중에 수정!!!!
                     db.collection("chat").document("2").collection("message").document(curTime).set(chatData);//firestore 에 데이터 저장
 //                     나중에 document "2" -> nickname 으로 수정!!
-                    Map<String, Object> map  = new HashMap<>();
-                    map.put("dateExample", new Timestamp(new Date()));
+                    ((ChatAdatper) mAdapter).addChat(chatData);//어댑터 데이터 전달
+                    mAdapter.notifyDataSetChanged();//업데이트
 
-                    db.collection("chat").document("2").collection("serverTimestamp").document().set(map);
-                    Log.d(TAG , "Server TimeStamp : " +  map);
+
+                    Map<String, Object> map = new HashMap<>();
+                    Timestamp timestamp = Timestamp.now();
+                    map.put("dateExample", timestamp);
+                    Timestamp.now().toDate();
+
+                    db.collection("chat").document("2").collection("serverTimestamp").document(timestamp.toString()).set(map);
+                    Log.d(TAG, "Server TimeStamp : " + timestamp.getNanoseconds() / 1000000);
+                    Log.d(TAG, "Server TimeStamp to Date() : " + Timestamp.now().toDate());
+                    db.collection("chat").document("2").collection("serverTimestamp").document(Timestamp.now().toDate().toString()).set(map);
+                    db.collection("chat").document("2").collection("serverTimestamp").document(Timestamp.now().toDate().toString()).set(map);
 
                     editText_chat.setText("");
 ////                    Log.d(TAG, "chatDataList is : " + chatDataList.get(0).getTime());
 
-                }else{
+                } else {
                     Log.d(TAG, "mag is null !! be careful ");
                 }
             }
         });
+
         colRef.addSnapshotListener(new EventListener<QuerySnapshot>() {//firestore 에 chat 컬렉션에 message 컬렉션의 문서의 변화가 감지되면 실행
+            //            상대방의 메시지를 받아옴
             @Override
             public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
                 String nickname_Q;
@@ -126,12 +138,13 @@ public class FragmentChat extends Fragment {
                     chat_time_Q = queryDocumentSnapshots.getDocuments().get(queryDocumentSnapshots.size() - 1).get("chat_time").toString();
                     content_Q = queryDocumentSnapshots.getDocuments().get(queryDocumentSnapshots.size() - 1).get("content").toString();
                     ChatData chatData = new ChatData(nickname_Q, content_Q, chat_time_Q);
-                    Log.d(TAG, "curNickNmae : " + nickname_Q);//최신 nickname_Q 내용 수신
-                    Log.d(TAG, "curChatTime : " + chat_time_Q);//최신 chat_time_Q 내용 수신
-                    Log.d(TAG, "curContent : " + content_Q);//최신 content_Q 내용 수신
-                    ((ChatAdatper) mAdapter).addChat(chatData);//어댑터 데이터 전달
-                    mAdapter.notifyDataSetChanged();//업데이트
-
+                    if (!nickname_Q.equals(my_nickname)) {//상대방의 chat이다.
+                        Log.d(TAG, "curNickNmae : " + nickname_Q);//최신 nickname_Q 내용 수신
+                        Log.d(TAG, "curChatTime : " + chat_time_Q);//최신 chat_time_Q 내용 수신
+                        Log.d(TAG, "curContent : " + content_Q);//최신 content_Q 내용 수신
+                        ((ChatAdatper) mAdapter).addChat(chatData);//어댑터 데이터 전달
+                        mAdapter.notifyDataSetChanged();//업데이트
+                    }
 
 
                 } else {
@@ -146,7 +159,7 @@ public class FragmentChat extends Fragment {
         mRecyclerView.setLayoutManager(mLayoutManager);//fragment chat 화면에 들어올 때 초기 1번만 실행됨.
 //        Log.d(TAG, "curTime : "+curTime);
         Log.d(TAG, "when this code run : ");
-        mAdapter = new ChatAdatper(chatDataList, getActivity().getApplicationContext(), nickname, curTime);
+        mAdapter = new ChatAdatper(chatDataList, getActivity().getApplicationContext(), my_nickname, curTime);
         mRecyclerView.setAdapter(mAdapter);
 
         return rootview;
